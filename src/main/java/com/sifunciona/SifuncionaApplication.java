b7c1f607bd701c6fdb3bcb325786f3d4a0c54a80
StevenValerio7 <stevenvalerio7@gmail.com>
2025-10-22 20:30:57 -0600

    primer commit

---
+++ src/main/java/com/sifunciona/SifuncionaApplication.java
@@ -0,0 +1,13 @@
+package com.sifunciona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifuncionaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SifuncionaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sifunciona/SifuncionaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sifunciona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SifuncionaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
